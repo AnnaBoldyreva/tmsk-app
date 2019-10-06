b58878ea880261c2fbc9b54a5944c28cf7fa78b2
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-06 18:28:05 +0300

    .map for Post component

--- src/Components/Profile/SuccesDiary/DiaryPost/Post.js
+++ src/Components/Profile/SuccesDiary/DiaryPost/Post.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './Post.module.css'
 
-const Post = () => {
+const Post = (props) => {
   return (
       <div className={styles.post}>
-         Post 1
+         <h3>{props.header}</h3>
+          <div>{props.message}</div>
+          <div>Likes: {props.likesCount}</div>
       </div>
 
   );
