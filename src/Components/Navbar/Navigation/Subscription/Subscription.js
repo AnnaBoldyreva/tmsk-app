dbffb517dc9a3a54c80508feb422b478a83101d9
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-09 13:56:14 +0300

    add description for Subscription component, after need break down into more components

--- src/Components/Navbar/Navigation/Subscription/Subscription.js
+++ src/Components/Navbar/Navigation/Subscription/Subscription.js
@@ -6,7 +6,21 @@ const Subscription = () => {
   return (
 
           <div className={styles.subscription}>
-              Subscription Page
+              <div><h2>Подписка</h2></div>
+
+              <div className={styles.unsubscribeButton}>Текущая карта: номер какрты через пропсы
+                 <br/>
+                  <button>Отменить подписку</button>
+              </div>
+
+              <div className={styles.payments}>Платежи
+              <ul>
+                  <li>Дата: тут будут пропсы</li>
+                  <li>Подписка: тут будут пропсы с кол-во месяцев</li>
+                  <li>Цена подписки: пропсы с ценой</li>
+              </ul>
+              </div>
+              <div></div>
 
           </div>
 
