bd3dc1e33583d65eec458e78c96269a63bb67973
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-09 13:43:41 +0300

    add NavLink to Dialog

--- src/Components/Navbar/Navigation/Dialogs/Dialog/Dialog.js
+++ src/Components/Navbar/Navigation/Dialogs/Dialog/Dialog.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import styles from './dialog.module.css'
+import {NavLink} from "react-router-dom";
 
 
 
 const Dialog = (props) => {
+    let path = '/dialogs/' + props.id;
   return (
 
       <div className={styles.dialog}>
-          <div>{props.name}</div>
+          <div className={styles.dialog + ' ' + styles.active}>
+              <NavLink to={path}>{props.name}</NavLink>
+          </div>
 
 
       </div>
