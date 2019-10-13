d71a989822be53d553093e631ec74ada20763c59
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-13 16:40:34 +0300

    add create Ref to Message component

--- src/Components/Navbar/Navigation/Dialogs/DialogMessages/Messages.js
+++ src/Components/Navbar/Navigation/Dialogs/DialogMessages/Messages.js
@@ -4,10 +4,23 @@ import styles from './messages.module.css'
 
 
 const Messages = (props) => {
+
+    let messageElement = React.createRef();
+    let sendMessage = ()=>{
+        let newText = messageElement.current.value;
+        alert(newText)
+    };
+
   return (
 
           <div className={styles.messages}>
-              <div>{props.message}</div>
+              <div>{props.message}
+              <br/>
+              <textarea ref={messageElement}>
+              </textarea>
+                  <button onClick={sendMessage}>send</button>
+
+              </div>
 
 
 
