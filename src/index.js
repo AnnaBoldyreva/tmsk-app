b6394ea0cb80c5a71cbfb4fd0d411add643a2015
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-29 17:12:05 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {BrowserRouter} from "react-router-dom";
 
 
 
- let rerender = (state) => {
+ let rerender = () => {
     ReactDOM.render(
         <BrowserRouter>
         <App state={store.getState()}
