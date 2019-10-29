3b2c826359bb6b92590ab45c4ef9a88bfed007fa
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-29 16:56:00 +0300

    add store

--- src/Redux/State.js
+++ src/Redux/State.js
@@ -1,46 +1,55 @@
-import {rerender} from "../redux";
 
-let state= {
-    messageData: [
-        {id:1, message: 'How are you?'},
-        {id:2, message: 'Can you help me?'},
-        {id:3, message: 'Show me your diet,please'}
-    ],
-    dialogDate: [
-        {id:1, name: 'Nate' },
-        {id:2, name: 'Serena' },
-        {id:3, name: 'Blair' },
-        {id:4, name: 'Dan' },
-        {id:5, name: 'Erick' }
-    ],
-    postData: [
-        {id:1, header:'My first week' , message:'It was hard.But I did it', likesCount: 321 },
-        {id:2, header:'My second week' , message:'It was not so bad' , likesCount:489  },
-        {id:3, header:'My third week' , message: 'I feel so good' , likesCount: 313 }
-    ],
-    profileData: [
-        {id: 1, name: 'Anna', age: 21 , country: 'Russia'}
-    ]
+let store = {
+    _state : {
+        messageData: [
+            {id: 1, message: 'How are you?'},
+            {id: 2, message: 'Can you help me?'},
+            {id: 3, message: 'Show me your diet,please'}
+        ],
+        dialogDate: [
+            {id: 1, name: 'Nate'},
+            {id: 2, name: 'Serena'},
+            {id: 3, name: 'Blair'},
+            {id: 4, name: 'Dan'},
+            {id: 5, name: 'Erick'}
+        ],
+        postData: [
+            {id: 1, header: 'My first week', message: 'It was hard.But I did it', likesCount: 321},
+            {id: 2, header: 'My second week', message: 'It was not so bad', likesCount: 489},
+            {id: 3, header: 'My third week', message: 'I feel so good', likesCount: 313}
+        ],
+        profileData: [
+            {id: 1, name: 'Anna', age: 21, country: 'Russia'}
+        ]
+    },
+    getState(){
+        return this._state
+    },
+    _callSubscribe(){
+        console.log('rerender');
+    },
+    addPost (postMessage) {
+        let newPost = {
+            id: 4,
+            header:'Yech',
+            message: postMessage,
+            likesCount: 0
+        };
+        this._state.postData.push(newPost);
+        // this._callSubscribe(this._state);
+    },
+    sendMessage (message){
+        let newMessage = {
+            id: 3,
+            message: message
+        };
+        this._state.messageData.push(newMessage)
+    },
+    subscribe(observer){
+        this._callSubscribe = observer;
+    }
+
 };
 
-export let addPost = (postMessage) => {
-    let newPost = {
-        id: 4,
-        header:'Yech',
-        message: postMessage,
-        likesCount: 0
-    };
-    state.postData.push(newPost);
-    rerender(state);
-
-};
-
-export let sendMessage = (Message)=>{
-    let newMessage = {
-        id: 3,
-        message: Message
-    };
-    state.messageData.push(newMessage)
-};
-
-export default state;
+export default store;
+window.store = store;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import ReactDOM from "react-dom";
+import store from './Redux/State';
+import {BrowserRouter} from "react-router-dom";
 
-import {rerender} from "./redux";
-import state from "./Redux/State";
 
-rerender(state);
 
+
+
+ let rerender = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+        <App state={store.getState()}
+             addPost={store.addPost.bind(store)}
+             sendMessage={store.sendMessage.bind(store)}/>
+        </BrowserRouter>, document.getElementById('root'));
+};
+
+
+rerender(store.getState());
+store.subscribe(rerender());
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
